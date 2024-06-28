77f9ade2b4e0c6c46cfffd073e9a763531f9aa2f
Vhyrro <vhyrro@gmail.com>
2024-06-28 20:23:48 +0200

    fix: ensure that entry exists in rocks.toml when checking updates

--- lua/rocks-edit/sources/updates.lua
+++ lua/rocks-edit/sources/updates.lua
@@ -1,16 +1,18 @@
 local nio = require("nio")
 
-require("rocks-edit.api").register(function(_, diagnostic)
+require("rocks-edit.api").register(function(toml, diagnostic)
     nio.run(function()
         local updates = require("rocks.state").outdated_rocks()
 
         for name, data in pairs(updates) do
-            diagnostic({
-                message = string.format("update available: `%s` -> `%s`", data.version, data.target_version),
-                path = { "plugins", name },
-                selector = "key",
-                severity = vim.diagnostic.severity.INFO,
-            })
+            if toml.plugins[name] then
+                diagnostic({
+                    message = string.format("update available: `%s` -> `%s`", data.version, data.target_version),
+                    path = { "plugins", name },
+                    selector = "key",
+                    severity = vim.diagnostic.severity.INFO,
+                })
+            end
         end
     end)
 end)

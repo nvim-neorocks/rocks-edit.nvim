813c592969c262a130488e6fcd8275371c98bb1e
Vhyrro <vhyrro@gmail.com>
2024-06-28 17:40:45 +0200

    fix: only refresh diagnostics if the rocks.toml has been modified

--- lua/rocks-edit/init.lua
+++ lua/rocks-edit/init.lua
@@ -6,7 +6,7 @@ local rocks_edit = {}
 ---@param buffer number The buffer ID.
 function rocks_edit.display_diagnostics(buffer)
     -- TODO(vhyrro): Make configurable
-    local sources = { "updates", "unsynced" }
+    local sources = { "unsynced", "updates" }
 
     for _, source in ipairs(sources) do
         require("rocks-edit.sources." .. source)
@@ -15,10 +15,26 @@ function rocks_edit.display_diagnostics(buffer)
     internal.check_rocks_toml(buffer)
 
     local group = vim.api.nvim_create_augroup("rocks-edit.nvim", { clear = true })
+    local modified
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = buffer,
+        group = group,
+        callback = function()
+            -- The `modified` variable cannot be checked after
+            -- the buffer has been written, so we check here instead.
+            modified = vim.bo[buffer].modified
+        end,
+    })
+
     vim.api.nvim_create_autocmd("BufWritePost", {
         buffer = buffer,
         group = group,
         callback = function()
+            if not modified then
+                return
+            end
+
             internal.check_rocks_toml(buffer)
         end,
     })

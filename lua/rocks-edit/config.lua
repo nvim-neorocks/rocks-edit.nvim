1fa4cff88d257be93b161292a95db0561abb3426
Vhyrro <vhyrro@gmail.com>
2024-06-29 17:44:56 +0200

    feat: proper configuration checks

--- lua/rocks-edit/config.lua
+++ lua/rocks-edit/config.lua
@@ -13,6 +13,24 @@ function config.configure_from_table(tbl)
     current_config = vim.tbl_deep_extend("force", default_config, tbl)
 end
 
+function config.validate_sources(sources)
+    for name, value in pairs(sources) do
+        if type(name) ~= "string" or type(value) ~= "boolean" or not default_config.sources[name] then
+            return false
+        end
+    end
+
+    return true
+end
+
+--- Quickly verifies a configuration. Full checks are performed in the healthcheck.
+---@param conf RocksEditConfig
+function config.validate(conf)
+    vim.validate({
+        sources = { conf.sources, config.validate_sources, "invalid list of sources provided. Run `:checkhealth rocks-edit.nvim` for more information." },
+    })
+end
+
 function config.configure()
     local toml = internal.get_toml()
 
@@ -25,6 +43,8 @@ function config.configure()
     if vim.g.rocks_nvim.edit then
         config.configure_from_table(vim.g.rocks_nvim.edit)
     end
+
+    config.validate(current_config)
 end
 
 function config.get()

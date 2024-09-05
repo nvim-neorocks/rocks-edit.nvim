6843868a52fbf0f1b67d40ebd776b20551a07e6d
Simi Falaye <simifalaye1@gmail.com>
2024-09-05 14:50:29 -0600

    fix: change rocks toml file detection to match symlinks also

--- ftplugin/toml.lua
+++ ftplugin/toml.lua
@@ -2,7 +2,7 @@ local ok, rocks = pcall(require, "rocks.api")
 
 assert(ok, "rocks.nvim is required for `rocks-edit.nvim` to function, please install it!")
 
-if vim.fn.expand("%") == rocks.get_rocks_toml_path() then
+if vim.uv.fs_realpath(vim.fn.expand("%")) == vim.uv.fs_realpath(rocks.get_rocks_toml_path()) then
     local rocks_edit = require("rocks-edit")
 
     rocks_edit.display_diagnostics(vim.api.nvim_get_current_buf())
